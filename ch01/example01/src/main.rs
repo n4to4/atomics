b8649eda7e9eb1442709e1895335f29b4f3ebea7
n4to4 <n4to4k@gmail.com>
2023-10-21 09:17:22 +0900

    scope

--- ch01/example01/src/main.rs
+++ ch01/example01/src/main.rs
@@ -1,6 +1,26 @@
 use std::thread;
 
 fn main() {
+    scoped_threads();
+}
+
+fn scoped_threads() {
+    let numbers = vec![1, 2, 3];
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            println!("length: {}", numbers.len());
+        });
+        s.spawn(|| {
+            for n in &numbers {
+                println!("{n}");
+            }
+        });
+    });
+}
+
+#[allow(dead_code)]
+fn thread_examples() {
     let t1 = thread::spawn(f);
     let t2 = thread::spawn(f);
 
@@ -8,6 +28,8 @@ fn main() {
 
     t1.join().unwrap();
     t2.join().unwrap();
+
+    //---
 
     let numbers = vec![1, 2, 3];
     thread::spawn(move || {
@@ -17,8 +39,20 @@ fn main() {
     })
     .join()
     .unwrap();
+
+    //---
+
+    let numbers = Vec::from_iter(0..=1000);
+    let t = thread::spawn(move || {
+        let len = numbers.len();
+        let sum = numbers.iter().sum::<usize>();
+        sum / len
+    });
+    let average = t.join().unwrap();
+    println!("average: {average}");
 }
 
+#[allow(dead_code)]
 fn f() {
     println!("Hello from another thread!");
 
