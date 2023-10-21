bc7cd6acecabb5e87493f87157d7a1ae7c2932e9
n4to4 <n4to4k@gmail.com>
2023-10-21 09:57:16 +0900

    leaking

--- ch01/example01/src/main.rs
+++ ch01/example01/src/main.rs
@@ -1,9 +1,19 @@
 use std::thread;
 
 fn main() {
-    scoped_threads();
+    leaking();
 }
 
+fn leaking() {
+    let x: &'static [i32; 3] = Box::leak(Box::new([1, 2, 3]));
+
+    let t1 = thread::spawn(move || dbg!(x));
+    let t2 = thread::spawn(move || dbg!(x));
+    t1.join().unwrap();
+    t2.join().unwrap();
+}
+
+#[allow(dead_code)]
 fn scoped_threads() {
     let numbers = vec![1, 2, 3];
 
