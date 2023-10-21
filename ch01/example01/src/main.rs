ef4dedb1797913537b0e7a1cdfc2cf5ddfa4fd23
n4to4 <n4to4k@gmail.com>
2023-10-21 09:09:16 +0900

    closure

--- ch01/example01/src/main.rs
+++ ch01/example01/src/main.rs
@@ -5,8 +5,18 @@ fn main() {
     let t2 = thread::spawn(f);
 
     println!("Hello from the main thread.");
+
     t1.join().unwrap();
     t2.join().unwrap();
+
+    let numbers = vec![1, 2, 3];
+    thread::spawn(move || {
+        for n in &numbers {
+            println!("{n}");
+        }
+    })
+    .join()
+    .unwrap();
 }
 
 fn f() {
