e5f2c9f0e9cc581a53845baa74eb5e91822807c3
n4to4 <n4to4k@gmail.com>
2023-10-24 09:17:32 +0900

    thread-parking

--- ch01/example01/src/main.rs
+++ ch01/example01/src/main.rs
@@ -1,7 +1,34 @@
 use std::thread;
 
 fn main() {
-    mutex();
+    parking();
+}
+
+#[allow(dead_code)]
+fn parking() {
+    use std::collections::VecDeque;
+    use std::sync::Mutex;
+    use std::time::Duration;
+
+    let queue = Mutex::new(VecDeque::new());
+    thread::scope(|s| {
+        // Consuming thread
+        let t = s.spawn(|| loop {
+            let item = queue.lock().unwrap().pop_front();
+            if let Some(item) = item {
+                dbg!(item);
+            } else {
+                thread::park();
+            }
+        });
+
+        // Producing thread
+        for i in 0.. {
+            queue.lock().unwrap().push_back(i);
+            t.thread().unpark();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
 }
 
 #[allow(dead_code)]
