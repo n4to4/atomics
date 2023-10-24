0ca185f3276c9f9af0c70e00ac95aafa746579dc
n4to4 <n4to4k@gmail.com>
2023-10-24 09:37:59 +0900

    condvar

--- ch01/example01/src/main.rs
+++ ch01/example01/src/main.rs
@@ -1,31 +1,37 @@
 use std::thread;
 
 fn main() {
-    parking();
+    parking_condvar();
 }
 
 #[allow(dead_code)]
-fn parking() {
+fn parking_condvar() {
     use std::collections::VecDeque;
-    use std::sync::Mutex;
+    use std::sync::{Condvar, Mutex};
     use std::time::Duration;
 
     let queue = Mutex::new(VecDeque::new());
+    let not_empty = Condvar::new();
+
     thread::scope(|s| {
         // Consuming thread
-        let t = s.spawn(|| loop {
-            let item = queue.lock().unwrap().pop_front();
-            if let Some(item) = item {
-                dbg!(item);
-            } else {
-                thread::park();
-            }
+        s.spawn(|| loop {
+            let mut q = queue.lock().unwrap();
+            let item = loop {
+                if let Some(item) = q.pop_front() {
+                    break item;
+                } else {
+                    q = not_empty.wait(q).unwrap();
+                }
+            };
+            drop(q);
+            dbg!(item);
         });
 
         // Producing thread
         for i in 0.. {
             queue.lock().unwrap().push_back(i);
-            t.thread().unpark();
+            not_empty.notify_one();
             thread::sleep(Duration::from_secs(1));
         }
     });
