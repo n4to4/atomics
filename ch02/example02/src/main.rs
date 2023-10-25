f7d15d2ca73b6cd46a5194ec6a51430ed830d327
n4to4 <n4to4k@gmail.com>
2023-10-25 17:10:27 +0900

    park

--- ch02/example02/src/main.rs
+++ ch02/example02/src/main.rs
@@ -6,6 +6,7 @@ use std::{
 
 fn main() {
     let num_done = AtomicUsize::new(0);
+    let main_thread = thread::current();
 
     thread::scope(|s| {
         // A background thread to process all 100 items.
@@ -13,17 +14,18 @@ fn main() {
             for i in 0..100 {
                 process_item(i); // Assuming this takes some time.
                 num_done.store(i + 1, Relaxed);
+                main_thread.unpark();
             }
         });
 
-        // The main thread shows status updates, every second.
+        // The main thread shows status updates.
         loop {
             let n = num_done.load(Relaxed);
             if n == 100 {
                 break;
             }
             println!("Working.. {n}/100 done");
-            thread::sleep(Duration::from_millis(1000));
+            thread::park_timeout(Duration::from_secs(1));
         }
     });
 
