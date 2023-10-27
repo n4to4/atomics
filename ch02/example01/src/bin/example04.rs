47c594b5c1f46b8b3f54e36060fa3d702503b887
n4to4 <n4to4k@gmail.com>
2023-10-27 15:54:03 +0900

    atomic

---
+++ ch02/example01/src/bin/example04.rs
@@ -0,0 +1,38 @@
+use std::{
+    sync::atomic::{AtomicUsize, Ordering::Relaxed},
+    thread,
+    time::Duration,
+};
+
+fn main() {
+    let num_done = &AtomicUsize::new(0);
+
+    thread::scope(|s| {
+        // Four background threads to process all 100 items, 25 each.
+        for t in 0..4 {
+            s.spawn(move || {
+                for i in 0..25 {
+                    process_item(t * 25 + i); // Assuming this takes some time.
+                    num_done.fetch_add(1, Relaxed);
+                }
+            });
+        }
+
+        // The main thread shows status updates, every second.
+        loop {
+            let n = num_done.load(Relaxed);
+            if n == 100 {
+                break;
+            }
+            println!("Working.. {n}/100 done");
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    println!("Done!");
+}
+
+fn process_item(n: i32) {
+    println!("processing... {n}");
+    thread::sleep(Duration::from_secs(2));
+}
