8912717cbf31ba9d895b52eda8ada1cde6dea30a
n4to4 <n4to4k@gmail.com>
2023-10-28 09:30:24 +0900

    rand

--- ch02/example01/src/bin/example05.rs
+++ ch02/example01/src/bin/example05.rs
@@ -1,4 +1,4 @@
-use std::sync::atomic::{AtomicU32, Ordering::Relaxed};
+use std::sync::atomic::{AtomicU32, AtomicU64, Ordering::Relaxed};
 
 fn main() {}
 
@@ -12,4 +12,22 @@ pub fn allocate_new_id() -> u32 {
             Err(v) => id = v,
         }
     }
+}
+
+pub fn get_key() -> u64 {
+    static KEY: AtomicU64 = AtomicU64::new(0);
+    let key = KEY.load(Relaxed);
+    if key == 0 {
+        let new_key = generate_random_key();
+        match KEY.compare_exchange(0, new_key, Relaxed, Relaxed) {
+            Ok(_) => new_key,
+            Err(k) => k,
+        }
+    } else {
+        key
+    }
+}
+
+fn generate_random_key() -> u64 {
+    rand::random()
 }
