a235c42ee88cf7aaee988013d6c12b354c53b265
n4to4 <n4to4k@gmail.com>
2023-11-06 09:30:19 +0900

    v2

---
+++ ch04/src/v2.rs
@@ -0,0 +1,17 @@
+use std::{cell::UnsafeCell, sync::atomic::AtomicBool};
+
+pub struct SpinLock<T> {
+    locked: AtomicBool,
+    value: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for SpinLock<T> where T: Send {}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
+        }
+    }
+}
