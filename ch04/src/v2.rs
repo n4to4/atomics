b6d967ee50981a52a56f06bd41614b483965537a
n4to4 <n4to4k@gmail.com>
2023-11-06 09:39:09 +0900

    spinlock

--- ch04/src/v2.rs
+++ ch04/src/v2.rs
@@ -1,4 +1,10 @@
-use std::{cell::UnsafeCell, sync::atomic::AtomicBool};
+use std::{
+    cell::UnsafeCell,
+    sync::atomic::{
+        AtomicBool,
+        Ordering::{Acquire, Release},
+    },
+};
 
 pub struct SpinLock<T> {
     locked: AtomicBool,
@@ -13,5 +19,21 @@ impl<T> SpinLock<T> {
             locked: AtomicBool::new(false),
             value: UnsafeCell::new(value),
         }
+    }
+
+    #[allow(clippy::mut_from_ref, clippy::needless_lifetimes)]
+    pub fn lock<'a>(&'a self) -> &'a mut T {
+        while self.locked.swap(true, Acquire) {
+            std::hint::spin_loop();
+        }
+        unsafe { &mut *self.value.get() }
+    }
+
+    /// # Safety
+    ///
+    /// The &mut T from lock() must be gone!
+    /// (And no cheating by keeping reference to fields of that T around!)
+    pub unsafe fn unlock(&self) {
+        self.locked.store(false, Release);
     }
 }
