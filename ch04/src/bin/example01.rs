8703ee38bd99ff622f1521745d5fb0c75ac37f27
n4to4 <n4to4k@gmail.com>
2023-11-06 09:52:53 +0900

    example

--- ch04/src/bin/example01.rs
+++ ch04/src/bin/example01.rs
@@ -1,3 +1,16 @@
+use spinlock::v3::SpinLock;
+use std::thread;
+
 fn main() {
-    let _lock = spinlock::v1::SpinLock::new();
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s| {
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
 }
