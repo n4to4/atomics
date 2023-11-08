6849409216fd2cba4b54183372593cd85189ec91
n4to4 <n4to4k@gmail.com>
2023-11-08 09:15:38 +0900

    one-shot

--- ch05/src/v2.rs
+++ ch05/src/v2.rs
@@ -1,4 +1,8 @@
-use std::{cell::UnsafeCell, mem::MaybeUninit, sync::atomic::AtomicBool};
+use std::{
+    cell::UnsafeCell,
+    mem::MaybeUninit,
+    sync::atomic::{AtomicBool, Ordering::Release},
+};
 
 pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
@@ -6,3 +10,19 @@ pub struct Channel<T> {
 }
 
 unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
+        }
+    }
+
+    /// # Safety
+    /// Only call this once!
+    pub unsafe fn send(&self, message: T) {
+        (*self.message.get()).write(message);
+        self.ready.store(true, Release);
+    }
+}
