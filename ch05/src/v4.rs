c0d7fc7718ca8653773addb95c073450b58aa7e2
n4to4 <n4to4k@gmail.com>
2023-11-09 15:46:57 +0900

    channel

--- ch05/src/v4.rs
+++ ch05/src/v4.rs
@@ -1,11 +1,21 @@
 use std::{
     cell::UnsafeCell,
     mem::MaybeUninit,
-    sync::{atomic::AtomicBool, Arc},
+    sync::{
+        atomic::{
+            AtomicBool,
+            Ordering::{Acquire, Relaxed, Release},
+        },
+        Arc,
+    },
 };
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    todo!()
+    let a = Arc::new(Channel {
+        message: UnsafeCell::new(MaybeUninit::uninit()),
+        ready: AtomicBool::new(false),
+    });
+    (Sender { channel: a.clone() }, Receiver { channel: a })
 }
 
 pub struct Sender<T> {
@@ -23,15 +33,21 @@ struct Channel<T> {
 unsafe impl<T> Sync for Channel<T> where T: Send {}
 
 impl<T> Sender<T> {
-    pub fn send(self, message: T) {}
+    pub fn send(self, message: T) {
+        unsafe { (*self.channel.message.get()).write(message) };
+        self.channel.ready.store(true, Release);
+    }
 }
 
 impl<T> Receiver<T> {
     pub fn is_ready(&self) -> bool {
-        false
+        self.channel.ready.load(Relaxed)
     }
 
     pub fn receive(self) -> T {
-        todo!()
+        if !self.channel.ready.swap(false, Acquire) {
+            panic!("no message available!");
+        }
+        unsafe { (*self.channel.message.get()).assume_init_read() }
     }
 }
