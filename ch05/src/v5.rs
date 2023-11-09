1b15215d60f43c8c8ba8811d3ef17e08aa724983
n4to4 <n4to4k@gmail.com>
2023-11-10 08:53:09 +0900

    channel

--- ch05/src/v5.rs
+++ ch05/src/v5.rs
@@ -1,6 +1,9 @@
 use std::{
-    cell::UnsafeCell, marker::PhantomData, mem::MaybeUninit, sync::atomic::AtomicBool,
-    thread::Thread,
+    cell::UnsafeCell,
+    marker::PhantomData,
+    mem::MaybeUninit,
+    sync::atomic::AtomicBool,
+    thread::{self, Thread},
 };
 
 pub struct Sender<'a, T> {
@@ -13,9 +16,32 @@ pub struct Receiver<'a, T> {
     _no_send: PhantomData<*const ()>,
 }
 
-struct Channel<T> {
+pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
     ready: AtomicBool,
 }
 
 unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
+        }
+    }
+
+    pub fn split<'a>(&'a mut self) -> (Sender<'a, T>, Receiver<'a, T>) {
+        *self = Self::new();
+        (
+            Sender {
+                channel: self,
+                receiving_thread: thread::current(),
+            },
+            Receiver {
+                channel: self,
+                _no_send: PhantomData,
+            },
+        )
+    }
+}
