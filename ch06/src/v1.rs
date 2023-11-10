ce99e43b684788deb771db42dab4fd104f6f60c3
n4to4 <n4to4k@gmail.com>
2023-11-11 08:36:18 +0900

    arc

---
+++ ch06/src/v1.rs
@@ -0,0 +1,36 @@
+use std::{ops::Deref, ptr::NonNull, sync::atomic::AtomicUsize};
+
+struct ArcData<T> {
+    ref_count: AtomicUsize,
+    data: T,
+}
+
+pub struct Arc<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+
+unsafe impl<T: Send + Sync> Send for Arc<T> {}
+unsafe impl<T: Send + Sync> Sync for Arc<T> {}
+
+impl<T> Arc<T> {
+    pub fn new(data: T) -> Arc<T> {
+        Arc {
+            ptr: NonNull::from(Box::leak(Box::new(ArcData {
+                ref_count: AtomicUsize::new(1),
+                data,
+            }))),
+        }
+    }
+
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+}
+
+impl<T> Deref for Arc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.data().data
+    }
+}
