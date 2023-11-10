2670a6d1e37e318a34b064e0df317b30b13ac9ac
n4to4 <n4to4k@gmail.com>
2023-11-11 08:56:31 +0900

    test

--- ch06/src/v1.rs
+++ ch06/src/v1.rs
@@ -62,3 +62,41 @@ impl<T> Drop for Arc<T> {
         }
     }
 }
+
+#[test]
+fn test() {
+    static NUM_DROPS: AtomicUsize = AtomicUsize::new(0);
+    struct DetectDrop;
+    impl Drop for DetectDrop {
+        fn drop(&mut self) {
+            NUM_DROPS.fetch_add(1, Relaxed);
+        }
+    }
+
+    // Create two Arcs sharing an object containing a string
+    // and a DetectDrop, to detect when it's dropped.
+    let x = Arc::new(("hello", DetectDrop));
+    let y = x.clone();
+
+    // Send x to another thread, and use it there.
+    let t = std::thread::spawn(move || {
+        assert_eq!(x.0, "hello");
+    });
+
+    // In parallel, y should still be usable here.
+    assert_eq!(y.0, "hello");
+
+    // Wait for the thread to finish.
+    t.join().unwrap();
+
+    // One Arc, x, should be dropped by now.
+    // We still have y, so the object shouldn't have been dropped yet.
+    assert_eq!(NUM_DROPS.load(Relaxed), 0);
+
+    // Drop the remaining `Arc`.
+    drop(y);
+
+    // Now that `y` is dropped too,
+    // the object should've been dropped.
+    assert_eq!(NUM_DROPS.load(Relaxed), 1);
+}
