268a3fbc66f8095f949f75c6186c83b6aab80ea6
n4to4 <n4to4k@gmail.com>
2023-11-13 09:42:55 +0900

    downgrade

--- ch06/src/v3.rs
+++ ch06/src/v3.rs
@@ -155,4 +155,26 @@ impl<T> Arc<T> {
         fence(Acquire);
         unsafe { Some(&mut *arc.data().data.get()) }
     }
+
+    pub fn downgrade(arc: &Self) -> Weak<T> {
+        let mut n = arc.data().alloc_ref_count.load(Relaxed);
+        loop {
+            if n == usize::MAX {
+                std::hint::spin_loop();
+                n = arc.data().alloc_ref_count.load(Relaxed);
+                continue;
+            }
+            assert!(n < usize::MAX - 1);
+            // Acquire synchronizes with get_mut's release-store.
+            if let Err(e) =
+                arc.data()
+                    .alloc_ref_count
+                    .compare_exchange_weak(n, n + 1, Acquire, Relaxed)
+            {
+                n = e;
+                continue;
+            }
+            return Weak { ptr: arc.ptr };
+        }
+    }
 }
