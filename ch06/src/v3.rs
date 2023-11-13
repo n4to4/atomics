db6b0a2b428613299c415e2927382a25f60d1028
n4to4 <n4to4k@gmail.com>
2023-11-13 09:19:27 +0900

    weak

--- ch06/src/v3.rs
+++ ch06/src/v3.rs
@@ -1,4 +1,13 @@
-use std::{cell::UnsafeCell, mem::ManuallyDrop, ptr::NonNull, sync::atomic::AtomicUsize};
+use std::{
+    cell::UnsafeCell,
+    mem::ManuallyDrop,
+    ops::Deref,
+    ptr::NonNull,
+    sync::atomic::{
+        fence, AtomicUsize,
+        Ordering::{Acquire, Relaxed, Release},
+    },
+};
 
 pub struct Arc<T> {
     ptr: NonNull<ArcData<T>>,
@@ -21,4 +30,56 @@ struct ArcData<T> {
     alloc_ref_count: AtomicUsize,
     /// The data. Dropped if there are only weak pointers left.
     data: UnsafeCell<ManuallyDrop<T>>,
+}
+
+impl<T> Arc<T> {
+    pub fn new(data: T) -> Arc<T> {
+        Arc {
+            ptr: NonNull::from(Box::leak(Box::new(ArcData {
+                alloc_ref_count: AtomicUsize::new(1),
+                data_ref_count: AtomicUsize::new(1),
+                data: UnsafeCell::new(ManuallyDrop::new(data)),
+            }))),
+        }
+    }
+
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+}
+
+impl<T> Deref for Arc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        // Safety: Since there's an Arc to the data,
+        // the data exists and may be shared.
+        unsafe { &*self.data().data.get() }
+    }
+}
+
+impl<T> Weak<T> {
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+}
+
+impl<T> Clone for Weak<T> {
+    fn clone(&self) -> Self {
+        if self.data().alloc_ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        Weak { ptr: self.ptr }
+    }
+}
+
+impl<T> Drop for Weak<T> {
+    fn drop(&mut self) {
+        if self.data().alloc_ref_count.fetch_add(1, Release) == 1 {
+            fence(Acquire);
+            unsafe {
+                drop(Box::from_raw(self.ptr.as_ptr()));
+            }
+        }
+    }
 }
