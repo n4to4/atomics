78bde9dba60ae6759c5adc0ec32fe8b440dd3205
n4to4 <n4to4k@gmail.com>
2023-11-13 09:07:27 +0900

    v3

---
+++ ch06/src/v3.rs
@@ -0,0 +1,24 @@
+use std::{cell::UnsafeCell, mem::ManuallyDrop, ptr::NonNull, sync::atomic::AtomicUsize};
+
+pub struct Arc<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+
+unsafe impl<T: Sync + Send> Send for Arc<T> {}
+unsafe impl<T: Sync + Send> Sync for Arc<T> {}
+
+pub struct Weak<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+
+unsafe impl<T: Sync + Send> Send for Weak<T> {}
+unsafe impl<T: Sync + Send> Sync for Weak<T> {}
+
+struct ArcData<T> {
+    /// Number of `Arc`s.
+    data_ref_count: AtomicUsize,
+    /// Number of `Weak`s, plus one if there are any `Arc`s.
+    alloc_ref_count: AtomicUsize,
+    /// The data. Dropped if there are only weak pointers left.
+    data: UnsafeCell<ManuallyDrop<T>>,
+}
