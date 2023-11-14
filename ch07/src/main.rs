c5662be015ad35e73aadd167fdb4700ac3a096a0
n4to4 <n4to4k@gmail.com>
2023-11-15 07:01:57 +0900

    black_box

--- ch07/src/main.rs
+++ ch07/src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     hint::black_box,
     sync::atomic::{AtomicU64, Ordering::Relaxed},
+    thread,
     time::Instant,
 };
 
@@ -8,6 +9,11 @@ static A: AtomicU64 = AtomicU64::new(0);
 
 fn main() {
     black_box(&A);
+
+    thread::spawn(|| loop {
+        black_box(A.load(Relaxed));
+    });
+
     let start = Instant::now();
     for _ in 0..1_000_000_000 {
         black_box(A.load(Relaxed));
