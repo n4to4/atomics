e67bc68d92c3c3125704967dc46e700669340324
n4to4 <n4to4k@gmail.com>
2023-11-21 10:29:11 +0900

    condvar

--- ch09/src/condvar/v1.rs
+++ ch09/src/condvar/v1.rs
@@ -1,4 +1,5 @@
-use atomic_wait::{wake_all, wake_one};
+use crate::mutex::v3::MutexGuard;
+use atomic_wait::{wait, wake_all, wake_one};
 use std::sync::atomic::{AtomicU32, Ordering::Relaxed};
 
 pub struct Condvar {
@@ -20,5 +21,19 @@ impl Condvar {
     pub fn notify_all(&self) {
         self.counter.fetch_add(1, Relaxed);
         wake_all(&self.counter);
+    }
+
+    pub fn wait<'a, T>(&self, guard: MutexGuard<'a, T>) -> MutexGuard<'a, T> {
+        let counter_value = self.counter.load(Relaxed);
+
+        // Unlock the mutex by dropping the guard,
+        // but remember the mutex so we can lock it again later.
+        let mutex = guard.mutex;
+        drop(guard);
+
+        // Wait, but only if the counter hasn't changed since unlocking.
+        wait(&self.counter, counter_value);
+
+        mutex.lock()
     }
 }
