67e05334f2d028f5443c627acf98633efe85f565
n4to4 <n4to4k@gmail.com>
2023-11-22 09:56:36 +0900

    deref

--- ch09/src/rwlock/v1.rs
+++ ch09/src/rwlock/v1.rs
@@ -1,4 +1,9 @@
-use std::{cell::UnsafeCell, sync::atomic::AtomicU32};
+use std::{
+    cell::UnsafeCell,
+    fmt::Write,
+    ops::{Deref, DerefMut},
+    sync::atomic::AtomicU32,
+};
 
 pub struct RwLock<T> {
     /// The number of readers, or u32::MAX if write-locked.
@@ -31,4 +36,17 @@ pub struct ReadGuard<'a, T> {
 
 pub struct WriteGuard<'a, T> {
     rwlock: &'a RwLock<T>,
+}
+
+impl<T> Deref for WriteGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.rwlock.value.get() }
+    }
+}
+
+impl<T> DerefMut for WriteGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.rwlock.value.get() }
+    }
 }
