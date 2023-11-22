0ea450bbcfae2af0158b61f0dc46b3e55b8b21ce
n4to4 <n4to4k@gmail.com>
2023-11-22 09:08:40 +0900

    condvar

--- ch09/src/condvar/v1.rs
+++ ch09/src/condvar/v1.rs
@@ -1,29 +1,37 @@
 use crate::mutex::v3::MutexGuard;
 use atomic_wait::{wait, wake_all, wake_one};
-use std::sync::atomic::{AtomicU32, Ordering::Relaxed};
+use std::sync::atomic::{AtomicU32, AtomicUsize, Ordering::Relaxed};
 
 pub struct Condvar {
     counter: AtomicU32,
+    num_waiters: AtomicUsize,
 }
 
 impl Condvar {
     pub const fn new() -> Self {
         Self {
             counter: AtomicU32::new(0),
+            num_waiters: AtomicUsize::new(0),
         }
     }
 
     pub fn notify_one(&self) {
-        self.counter.fetch_add(1, Relaxed);
-        wake_one(&self.counter);
+        if self.num_waiters.load(Relaxed) > 0 {
+            self.counter.fetch_add(1, Relaxed);
+            wake_one(&self.counter);
+        }
     }
 
     pub fn notify_all(&self) {
-        self.counter.fetch_add(1, Relaxed);
-        wake_all(&self.counter);
+        if self.num_waiters.load(Relaxed) > 0 {
+            self.counter.fetch_add(1, Relaxed);
+            wake_all(&self.counter);
+        }
     }
 
     pub fn wait<'a, T>(&self, guard: MutexGuard<'a, T>) -> MutexGuard<'a, T> {
+        self.num_waiters.fetch_add(1, Relaxed);
+
         let counter_value = self.counter.load(Relaxed);
 
         // Unlock the mutex by dropping the guard,
@@ -33,6 +41,8 @@ impl Condvar {
 
         // Wait, but only if the counter hasn't changed since unlocking.
         wait(&self.counter, counter_value);
+
+        self.num_waiters.fetch_sub(1, Relaxed);
 
         mutex.lock()
     }
