e9087d6b5b3f9c239ae86c9b954c92e456f241d3
n4to4 <n4to4k@gmail.com>
2023-11-22 09:43:37 +0900

    rwlock

--- ch09/src/rwlock/v1.rs
+++ ch09/src/rwlock/v1.rs
@@ -1,6 +1,34 @@
 use std::{cell::UnsafeCell, sync::atomic::AtomicU32};
 
 pub struct RwLock<T> {
+    /// The number of readers, or u32::MAX if write-locked.
     state: AtomicU32,
     value: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for RwLock<T> where T: Send + Sync {}
+
+impl<T> RwLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn read(&self) -> ReadGuard<T> {
+        todo!()
+    }
+
+    pub fn write(&self) -> WriteGuard<T> {
+        todo!()
+    }
+}
+
+pub struct ReadGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
+}
+
+pub struct WriteGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
 }
